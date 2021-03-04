4e829bbcdfd6b6c64045ed9065bde1b20ce3ed55
Alex S <sannnao.developer@gmail.com>
2021-03-05 01:21:51 +0300

    init: start Gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: "My_CV",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    "gatsby-plugin-styled-components",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+};
